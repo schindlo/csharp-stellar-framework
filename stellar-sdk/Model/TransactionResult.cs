1ce19263d8cdbec6681e4b12e4caaeb4127772cf
Oliver Schindler <oliver@schindler.gdn>
2018-01-31 22:38:06 +0100

    add TransactionResult model

---
+++ stellar-sdk/Model/TransactionResult.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+
+namespace StellarSdk.Model
+{
+    public class TransactionResult
+    {
+        [JsonProperty("_links")]
+        public Links Links { get; set; }
+
+        [JsonProperty("hash")]
+        public string Hash { get; set; }
+
+        [JsonProperty("ledger")]
+        public long Ledger { get; set; }
+
+        [JsonProperty("envelope_xdr")]
+        public string EnvelopeXdr { get; set; }
+
+        [JsonProperty("result_xdr")]
+        public string ResultXdr { get; set; }
+
+        [JsonProperty("result_meta_xdr")]
+        public string ResultMetaXdr { get; set; }
+
+        public static TransactionResult FromJson(string json) => JsonConvert.DeserializeObject<TransactionResult>(json, Converter.Settings);
+
+        public static string ToJson(TransactionResult self) => JsonConvert.SerializeObject(self, Converter.Settings);
+    }
+}
